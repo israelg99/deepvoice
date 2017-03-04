96958955d108f9d8edd6bbffe072181de8294113
Israel Gilyadov <israelg99@gmail.com>
2017-03-04 14:46:28 +0200

    Implement CMUDict loader

---
+++ deepvoice/data/character_table.py
@@ -0,0 +1,47 @@
+import numpy as np
+import itertools
+import re
+
+# based on https://github.com/fchollet/keras/blob/master/examples/addition_rnn.py
+class CharacterTable(object):
+    '''
+    Given a set of characters:
+    + Encode them to a one hot integer representation
+    + Decode the one hot integer representation to their character output
+    + Decode a vector of probabilities to their character output
+    '''
+
+    def __init__(self, chars='', maxlen=None, null_char=' ', left_pad=False):
+        self.chars = sorted(set([null_char] + list(chars)))
+        self.char_indices = dict((c, i) for i, c in enumerate(self.chars))
+        self.indices_char = dict((i, c) for i, c in enumerate(self.chars))
+        self.maxlen = maxlen
+        self.left_pad = left_pad
+        self.null_char = null_char
+
+    def fit(self, Cs, null_char=' '):
+        """Determine chars and maxlen by fitting to data"""
+        self.chars = sorted(set(itertools.chain([null_char], *Cs)))
+        self.char_indices = dict((c, i) for i, c in enumerate(self.chars))
+        self.indices_char = dict((i, c) for i, c in enumerate(self.chars))
+        self.maxlen = max(len(c) for c in Cs)
+        self.null_char = null_char
+
+    def encode(self, Cs, maxlen=None):
+        """Pass in an array of arrays to convert to integers"""
+        maxlen = maxlen if maxlen else self.maxlen
+        n = len(Cs)
+        X = np.zeros((n, maxlen, len(self.chars)), dtype=np.bool)
+        for j, C in enumerate(Cs):
+            if self.left_pad:
+                C = [self.null_char] * (maxlen - len(C)) + list(C)
+            else:
+                C = list(C) + [self.null_char] * (maxlen - len(C))
+                for i, c in enumerate(C):
+                    X[j, i, self.char_indices[c]] = True
+        return X
+
+    def decode(self, Xs, calc_argmax=True):
+        if calc_argmax:
+            Xs = Xs.argmax(axis=-1)
+        return np.array(list([self.indices_char[x] for x in X] for X in Xs))
---
+++ deepvoice/data/cmudict.py
@@ -0,0 +1,78 @@
+import numpy as np
+import itertools
+import re
+
+from keras.utils.data_utils import get_file
+from sklearn.model_selection import train_test_split
+
+from deepvoice.util.data_util import CharacterTable
+
+def get_cmudict(origin='https://raw.githubusercontent.com/cmusphinx/cmudict/master/cmudict.dict', test_size=0.33, verbose=False, maxlen_x=None, maxlen_y=None, blacklist='().0123456789', max_phonemes=np.inf, max_chars=np.inf, seed=42):
+    """
+    Process CMU pronounciation dictionary as one-hot encoded grapheme and phoneme data
+    # Output
+        (X_train, y_train): One-hot encoded graphemes and phonemes
+        (X_test, y_test): Test data
+        (xtable, ytable): Charecter en/decoding tables
+    # Arguments
+        seed: random seed for data split and shuffle
+        test_size: fraction of data to set aside for testing
+        verbose: print messages about data processing
+        maxlen_x: crop and pad grapheme sequences to this length
+        maxlen_y: crop and pad phoneme sequences to this length
+        max_phonemes: restrict data to this <=max_phonemes
+        max_chars: restrict data to this <=max_charectors
+        blacklist: remove words with these charectors e.g. HOUSE(2) for the second varient of house
+    """
+
+    cmudict_path = get_file("cmudict-py", origin=origin, untar=False)
+
+    # load data
+    X, y= [], []
+    for line in open(cmudict_path,'r').readlines():
+        word, pron = line.strip().split(' ',1)
+        X.append(list(word))
+        y.append(pron.split(' '))
+    X = np.array(X)
+    y = np.array(y)
+    if verbose:  print('loaded {} entries from cmu_dict'.format(len(X)))
+
+    # compile blacklist
+    p=re.compile('[%s]'%(blacklist))
+
+    # filter out duplicate entries like 'HOUSE(2)'
+    X, y = zip(*[(x,y) for x,y in zip(X,y) if not bool(p.findall(''.join(x)))])
+    if verbose:  print('removed duplicate entries leaving {}'.format(len(X)))
+
+    # filter out complex entries
+    X, y = zip(*[(x,y) for x,y in zip(X,y) if len(y)<=max_phonemes and len(x)<=max_chars])
+    if verbose:  print('restricted to less than {} phonemes leaving {} entries'.format(max_phonemes, len(X)))
+
+    # split data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=seed)
+
+    # encode x and y and pad them
+    xtable, X_train, X_test = _encode_chartable(X_train, X_test, maxlen_x)
+    ytable, y_train, y_test = _encode_chartable(y_train, y_test, maxlen_y)
+
+    if verbose:
+        print('X_train shape:', X_train.shape)
+        print('y_train shape:', y_train.shape)
+
+        print('X_test shape:', X_test.shape)
+        print('y_test shape:', y_test.shape)
+
+    return (X_train, y_train), (X_test, y_test), (xtable, ytable)
+
+def _encode_chartable(train, test, maxlen=None):
+    load = train
+
+    test_empty = len(test) == 0
+    if not test_empty:  load += test
+
+    table = CharacterTable()
+    table.fit(load)
+
+    if maxlen: table.maxlen = maxlen
+
+    return table, table.encode(train), np.empty(0) if test_empty else table.encode(test)
